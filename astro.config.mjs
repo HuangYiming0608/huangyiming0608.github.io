de960fb62f7cb5acd89051c38ba04be367547e97
HuangYiming0608 <133227329+HuangYiming0608@users.noreply.github.com>
2024-06-10 10:18:49 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ymhuang.eu.org',
-	integrations: [mdx(), sitemap()],
+	site: 'https://huangyiming0608.github.io/blog',
+        base: 'Blog',
 });

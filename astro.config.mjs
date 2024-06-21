1938ab5ca2bf9684c2f9f4799ed02149842144f5
yiming huang <you0608@yeah.net>
2024-06-21 21:14:47 +0800

    LATEX SURPPORT

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import pagefind from 'astro-pagefind';
-import sitemap from '@astrojs/sitemap';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://huangyiming0608.github.io',
-	integrations: [pagefind()],
+	integrations: [pagefind(),mdx()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	}
+
 });

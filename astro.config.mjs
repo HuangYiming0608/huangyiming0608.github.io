f39e38f2cb1483759cc5ad5fea21e4cd71227ef0
yiming huang <you0608@yeah.net>
2024-06-21 14:20:29 +0800

    SEARCH

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import pagefind from 'astro-pagefind';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://huangyiming0608.github.io',
+	integrations: [pagefind()],
 });

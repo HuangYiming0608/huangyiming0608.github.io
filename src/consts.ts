284455cb1885737937b6ab6340cff1b80c9abef5
HuangYiming0608 <133227329+HuangYiming0608@users.noreply.github.com>
2024-06-22 13:51:57 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'YMHuang Blog';
-export const SITE_TITLE_PAGE = 'YMHuang Blog | 穷人不止玩硬件，安资为钢铸软件';
-export const SITE_DESCRIPTION = '穷人不止玩硬件，安资为钢铸软件';
+export const SITE_TITLE_PAGE = 'YMHuang Blog | 穷人不绝玩硬件，安资为钢铸软件';
+export const SITE_DESCRIPTION = '穷人不绝玩硬件，安资为钢铸软件';

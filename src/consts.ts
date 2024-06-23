95afb9df0d693ef722c3ea97b4d1ad42f5060089
yiming huang <study0608@126.com>
2024-06-23 21:26:12 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-export const SITE_TITLE = 'YMHuang Blog | 穷人不绝玩硬件，安资为钢铸软件';
+export const SITE_TITLE_PAGE = 'YMHuang Blog | 穷人不绝玩硬件，安资为钢铸软件'
+export const SITE_TITLE = 'YMHuang Blog';
 export const SITE_DESCRIPTION = '穷人不绝玩硬件，安资为钢铸软件';

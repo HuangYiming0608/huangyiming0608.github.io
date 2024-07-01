32e0ae48b08ed6f7f595d8e6396b67f6d5ef55dc
yiming huang <study0608@126.com>
2024-07-01 18:10:12 +0800

    SSL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,16 @@ import pagefind from 'astro-pagefind';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://huangyiming0608.github.io',
-	integrations: [pagefind(),mdx()],
-	markdown: {
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeKatex],
-	}
-
+  site: 'https://huangyiming0608.github.io',
+  integrations: [pagefind(), mdx()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  },
+  output: "server",
+  adapter: cloudflare()
 });
